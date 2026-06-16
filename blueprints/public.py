ef055de3994f230bea741adf0471925455527eca
erickboechat <erickboechat@gmail.com>
2026-06-16 10:14:11 -0300

    chore: remove FAQ test page and admin prototype

--- blueprints/public.py
+++ blueprints/public.py
@@ -154,14 +154,3 @@ def llms_txt():
     )
     resp = Response(content, mimetype='text/plain')
     return resp
-
-
-@public_bp.route('/faq-teste')
-def faq_teste():
-    produto = get_produto(1)
-    if not produto:
-        produto = get_produto(2)
-    if not produto:
-        from flask import abort
-        abort(404)
-    return render_template('faq_test.html', produto=produto)
